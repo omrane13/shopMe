d5b89784b84f5426f9cfe273cedebe46568811fe
omrane13 <143047533+omrane13@users.noreply.github.com>
2025-08-28 12:19:55 +0100

    Delete Routes

--- Routes/AuthRouter.js
+++
@@ -1,9 +0,0 @@
-const { signup, login} = require('../Controllers/AuthController');
-const { signupValidation, loginValidation } = require('../Middleware/AuthValidation');
-const router =require ('express').Router();
-
-
-router.post('/login', loginValidation, login);
-router.post('/signup', signupValidation,signup);
-
-module.exports= router;

98c9afa4393b7231975132bbf32668d4523a5769
omrane13 <143047533+omrane13@users.noreply.github.com>
2025-08-28 12:26:46 +0100

    Delete src

--- src/components/FormatCurrency.js
+++
@@ -1,9 +0,0 @@
-const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
-  currency: "USD",
-  style: "currency",
-});
-const FormatCurrency = (number) => {
-  return CURRENCY_FORMATTER.format(number);
-};
-
-export default FormatCurrency;
